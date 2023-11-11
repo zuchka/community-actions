05663b4ed200f8ebbb8f817cd9fa8a9fe60a4f83
zuchka <zuchka@zuchkas-MacBook-Pro.local>
2023-11-11 12:37:51 -1000

    remove github package

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,19 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 
 const org = core.getInput('org', { required: true })
 const username = core.getInput('username', { required: true })
 const token = core.getInput('token', { required: true })
 
-const octokit = new (github.getOctokit as any)(token)
-
 export async function run(): Promise<void> {
-  try {
-    const isMember = await octokit.rest.orgs.checkMembership({
-      org,
-      username
-    })
-    if (isMember.status === 204) {
-      core.setOutput('result', isMember)
+  const url = `https://api.github.com/orgs/${org}/members/${username}`
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: token,
+      Accept: 'application/vnd.github+json',
+      'X-GitHub-Api-Version': '2022-11-28'
     }
-  } catch (error: any) {
-    if (error.status === 404) {
-      core.setOutput('result', '404')
-    } else {
-      console.error(
-        `An error occurred while checking if the repository is starred: ${error}`
-      )
-    }
-  }
+  })
+  console.log(response.status)
+  core.setOutput('result', response.status === 204 ? 'true' : 'false')
 }
