5222c0ebac4444d1d1896e121b8c58dbe4f79865
zuchka <zuchka@zuchkas-MacBook-Pro.local>
2023-11-11 14:09:14 -1000

    fix bool2

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import * as core from '@actions/core'
 const teammates = core.getInput('teammates', { required: true })
 const username = core.getInput('username', { required: true })
 
-export function run(): void {
+export function run(): any {
   const team: string[] = teammates.split('|')
   const isMember: boolean = team.includes(username)
-  core.setOutput('result', isMember)
+  core.setOutput('result', isMember ? 'true' : 'false')
 }
