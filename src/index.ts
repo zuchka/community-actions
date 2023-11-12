48b4b75976e3edfdb7259fc2f016665487fa4503
zuchka <zuchka@zuchkas-MacBook-Pro.local>
2023-11-11 14:17:49 -1000

    fix bool3

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,8 @@ const username = core.getInput('username', { required: true })
 export function run(): any {
   const team: string[] = teammates.split('|')
   const isMember: boolean = team.includes(username)
-  core.setOutput('result', isMember ? 'true' : 'false')
+  core.setOutput("result", isMember ? "true" : "false");
+  console.log('teammates =' + teammates)
+  console.log('team =' + team)
+  console.log('isMember =' + isMember)
 }
