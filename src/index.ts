ac0f60f78a9707df51e913c179b49807ae8c0fcd
zuchka <zuchka@zuchkas-MacBook-Pro.local>
2023-11-11 21:41:07 -1000

    new logic

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,46 @@ const username = core.getInput('username', { required: true })
 export function run(): any {
   const team = teammates.split('|')
   const isMember = team.includes(username)
-  console.log(isMember)
-  const foo = process.env.GITHUB_EVENT_NAME
-  console.log(foo)
-  const GHContext = process.env.GITHUB_CONTEXT
-  console.log(GHContext)
+  const GHEvent = process.env.GITHUB_EVENT_NAME
+  const GHRepo = process.env.GITHUB_REPO
+  const GHContext: any = process.env.GITHUB_CONTEXT
+  if (GHEvent == 'issue') {
+    const url = GHContext.event.issue.html_url
+    const title = GHContext.event.issue.title
+    const body = GHContext.event.issue.body
+    console.log(url)
+    console.log(title)
+    console.log(body)
+    postData(
+      'https://discord.com/api/webhooks/886039948032090152/TN0AU9rQs3bzWfIR-enPZp9xAW2XeOzYiCQH4Y_W6MX-ABjKaKzsJOTp_psayU_Z8H-f',
+      {
+        username: 'G Bot',
+        avatar_url: 'https://i.imgur.com/4M34hi2.png',
+        content: `An external contributor just created a new ${GHEvent} in ${GHRepo}.`,
+        embeds: [
+          {
+            title: title,
+            url: url,
+            description: body,
+            color: 15258703
+          }
+        ]
+      }
+    )
+  }
   core.setOutput('result', isMember ? 'true' : 'false')
 }
 
 run()
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  console.log(response.status)
+  return response.status
+}
