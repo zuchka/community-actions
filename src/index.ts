10d06cc20a0ca78d4fe0a1cc4e72482b7d4ec6c3
zuchka <>
2023-11-14 15:36:57 -1000

    test PR url

--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,34 @@ export function run(): any {
   const ghIssueUrl = stripQuotes(process.env.GITHUB_ISSUE_URL)
   const ghPrUrl = stripQuotes(process.env.GITHUB_PR_URL)
   const ghEvent = stripQuotes(process.env.GITHUB_EVENT_NAME)
-
-  const ghUrl = ghEvent === 'pull_request' ? ghPrUrl : ghIssueUrl
+  console.log('issue URL =' + ghIssueUrl)
+  console.log('Pr url ' + ghPrUrl)
+  const ghUrl = ghEvent == 'pull_request' ? ghPrUrl : ghIssueUrl
   core.setOutput('result', isMember ? 'true' : 'false')
+  console.log('set URL = ' + ghUrl)
+  //   if (!isMember) {
+  //     postData(webhook, {
+  //       username: 'Railway Community Bot',
+  //       avatar_url: 'https://railway.app/brand/logo-dark.png',
+  //       content: `new community activity in ${ghRepo}:\n\n${ghUrl}\n\n`,
+  //       embeds: null
+  //     })
+  //     console.log('sending webhook for: ' + ghUrl + ' ...')
+  //   } else {
+  //     console.log('Skipping webhook: author is an org member')
+  //   }
+  // }
 
-  if (!isMember) {
-    postData(webhook, {
-      username: 'Railway Community Bot',
-      avatar_url: 'https://railway.app/brand/logo-dark.png',
-      content: `new community activity in ${ghRepo}:\n\n${ghUrl}\n\n`,
-      embeds: null
-    })
-    console.log('sending webhook for: ' + ghUrl + ' ...')
-  } else {
-    console.log('Skipping webhook: author is an org member')
-  }
-}
-
-async function postData(url = '', data = {}) {
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  console.log('Webhook sent. response code = ' + response.status)
-  return response.status
+  // async function postData(url = '', data = {}) {
+  //   const response = await fetch(url, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(data)
+  //   })
+  //   console.log('Webhook sent. response code = ' + response.status)
+  //   return response.status
 }
 
 function stripQuotes(envVar: string | undefined) {
